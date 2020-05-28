5dd9f812dcf9d8d68fed921d431fe23b9ab31824
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 20:31:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,19 @@ end
 
 def assign_rooms(attendees)
   attendees.map.with_index(1) do |attendees,index|
-    "Hello, #{"
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(names).each do |assign_rooms|
+    puts assign_rooms
+  end
+end
+
  
 
