4b1b08032e875a52f4cb2aa6ae8098abd520d47f
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 17:21:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,20 @@
 badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return"Hello, my name is #{name}."
 end
 
-# badges.each do |badge|
-   # puts "Hello, my name is #{badge}."
- ##end
+def batch_badge_creator(badges)
+  badges.map {|name|"Hello, my name is #{name}."}
+   # return
+end
+
+def assign_rooms
   
+end
+
+def printer
+end
+ 
+  #(1..4).map { |i| i*i } 
 
