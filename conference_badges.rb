34cff3f7fd635438594b7026530343413da72331
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 20:27:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,13 @@
 def badge_maker(name)
-  "Hello, my nname is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   names.map do |name|
-    ""
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms()
  
 
