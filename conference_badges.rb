f8edb00399864e4612278ed09179bd4053d9d248
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 18:45:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,12 +14,20 @@ def assign_rooms(speakers)
   output=[]
   speakers.each do |speaker|
     output.push("Hello,#{speaker}! you'll be assigned to room #{output.size+1}!")
-  room_assignments.map.with_index{|name,room_assignments|"Hello, #{name}! You'll be assigned to #{room_assignments}!"} 
-  
+  end
+  output
 end
 
-def printer
+def printer(names)
+  badges=batch_badge_creator(names)
+  rooms=assign_rooms(names)
   
+  badges.each do |badge|
+    puts badge
+  end
+  rooms.each do |room|
+    puts room
+  end
 end
  
   #(1..4).map { |i| i*i } 
